31e937cd38f8ee2d59b791b712605cc837185b98
Mohamed AbdElaziz Awad <mido85only@gmail.com>
2024-10-15 22:01:41 +0300

    Initial Commit

---
+++ SwiftUiPractice/SwiftUiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiPracticeApp.swift
+//  SwiftUiPractice
+//
+//  Created by mohammed on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
