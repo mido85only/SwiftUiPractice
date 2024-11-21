8fb9368321ee48ef08fad7f57767222bc67b9524
Mo.Aziz <mohamed.aziz@systemira.net>
2024-11-21 16:58:58 +0200

    small changes

--- SwiftUiPractice/Spotify/Core/SpotifyHomeView.swift
+++ SwiftUiPractice/Spotify/Core/SpotifyHomeView.swift
@@ -144,7 +144,14 @@ struct SpotifyHomeView: View {
                 ScrollView(.horizontal) {
                     HStack(alignment: .top, spacing:16) {
                         ForEach(row.products) { product in
-                            ImageTitleRowCell(imageSize: 120, imageName: product.firstImage, title: product.title)
+                            ImageTitleRowCell(
+                                imageSize: 120,
+                                imageName: product.firstImage,
+                                title: product.title
+                            )
+                            .asButton(.press) {
+                                
+                            }
                         }
                     }
                     .padding(.horizontal , 16)
